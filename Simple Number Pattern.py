6c0eef6861c813b94481c958134578e539a646b4
Pritisurve1 <86795817+Pritisurve1@users.noreply.github.com>
2024-02-16 14:52:23 +0530

    Simple Number Pattern.py

--- Simple Number Pattern.py
+++ Simple Number Pattern.py
@@ -1,12 +1,14 @@
-#pyramid with ascending numbers
+#pyramid with ascending numbers(PART 1)
+
 rows = 8
 for i in range(1, rows+1):  
     for j in range(1, i + 1):  
         print(j, end=' ')  
     print('')  
-    
-    # number pyramid with diamond shape
-row = 15#
+
+
+# number pyramid with diamond shape(PART 2)
+row = 15
 for i in range(1, row + 1):
   print(" " * (row - i), end="")
     for k in range(1, i * 2):
